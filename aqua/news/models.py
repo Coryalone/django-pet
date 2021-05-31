baa8db2a37b62043f6db3196ea0985067e5432e6
Balaraphon <Balaraphon@yandex.ru>
2021-05-31 14:36:38 +0300

    Отобразил описанную таблицу в панели Администратора

--- aqua/news/models.py
+++ aqua/news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Articles(models.Model):
     title = models.CharField('Название', max_length=50)
     anons = models.CharField('Анонс', max_length=50)
@@ -9,3 +10,6 @@ class Articles(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
