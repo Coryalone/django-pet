91ca08a301f490dabc70fa8c6555f197081d27f0
Balaraphon <Balaraphon@yandex.ru>
2021-06-01 16:03:57 +0300

    Добавляю динамические страницы

--- aqua/news/views.py
+++ aqua/news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Articles
 from .forms import ArticlesForm
+from django.views.generic import DetailView
 
 
 def news_home(request):
@@ -8,7 +9,14 @@ def news_home(request):
     return render(request, 'news/news_home.html', {'news': news})
 
 
+class NewsDetailView(DetailView):
+    model = Articles
+    template_name = 'news/details_view.html'
+    context_object_name = 'article'
+
+
 def create(request):
+    error = ''
     if request.method == 'POST':
         form = ArticlesForm(request.POST)
         if form.is_valid():
